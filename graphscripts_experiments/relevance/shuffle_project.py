3c93f7d8a61e1b790f8761bdda9f43c422d03d7c
Marvinmw <1300623733@qq.com>
2022-03-10 16:00:57 +0100

    rename job folder

--- graphscripts_experiments/relevance/shuffle_project.py
+++
@@ -1,8 +0,0 @@
-import random
-import json
-projects = ["Cli", "Chart", "Closure", "Compress", "Csv", "Gson", "JacksonCore", "JacksonDetabind", "JacksonXml", "Jsoup", "JxPath", "Lang", "Math", "Mockito", "Time"]
-random.shuffle( projects )
-pre_projects = projects[:int(len(projects)*0.6)]
-remaining_projects = projects[int(len(projects)*0.6):]
-json.dump( pre_projects, open("pre_project.json", "w") )
-json.dump( remaining_projects, open("remaining_projects.json", "w") )
