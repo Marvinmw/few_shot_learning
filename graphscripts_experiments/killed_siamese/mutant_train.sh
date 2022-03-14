a0dcc9ba253222026c935722cc73fb10efd77f91
Marvinmw <1300623733@qq.com>
2022-03-14 16:55:13 +0100

    data

--- graphscripts_experiments/killed_siamese/mutant_train.sh
+++ graphscripts_experiments/killed_siamese/mutant_train.sh
@@ -5,7 +5,7 @@
 #SBATCH -G 1
 #SBATCH --time=2-00:00:00
 #SBATCH --qos=normal
-#SBATCH -J scl_r
+#SBATCH -J scl_k
 #SBATCH --mail-type=end,fail
 #SBATCH --mail-user=wei.ma@uni.lu
 #SBATCH -o %x-%j.log
@@ -17,9 +17,8 @@ num_class=2
 # collections csv io lang text
 for test_project in collections csv io lang text
 do
-output_folder=results/transfer_siamese_relevance/mutants_${num_class}_rm_${test_project}/context
+output_folder=results/transfer_siamese_killed/mutants_${num_class}_rm_${test_project}/context
 bash run.sh gat "pretrained_models/context/gat/model_0" ${output_folder}/ attention $device $num_class $test_project
 done
-
 
 
