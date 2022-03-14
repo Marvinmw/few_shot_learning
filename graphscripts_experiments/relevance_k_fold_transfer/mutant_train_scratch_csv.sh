503678ec850213d4cd68d31f16d5311ff98ad89f
Marvinmw <1300623733@qq.com>
2022-03-14 13:10:58 +0100

    transferweights

---
+++ graphscripts_experiments/relevance_k_fold_transfer/mutant_train_scratch_csv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH -n 3
+#SBATCH -N 1
+#SBATCH -p gpu
+#SBATCH -G 1
+#SBATCH --time=2-00:00:00
+#SBATCH --qos=normal
+#SBATCH -J csv_r
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=wei.ma@uni.lu
+#SBATCH -o %x-%j.log
+
+conda activate graph
+device=0
+num_class=2
+
+for train_project in csv 
+do
+for loss in  CE SCL
+do
+output_folder=results/supervised_relevance_transferweights/${loss}/mutants_${num_class}_loss_${loss}_train_${train_project}/context
+bash run.sh gat "pretrained_models/context/gat/model_0" ${output_folder}/ attention $device $num_class $loss $train_project results/transfer_supervised_relevance/CE/mutants_2_loss_CE_rm_csv/context/gat/saved_model.pt
+done
+done
+
+
