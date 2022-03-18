fdf22b60aa6291b4a6c8542119fb10e531dde9bc
Marvinmw <1300623733@qq.com>
2022-03-18 15:18:48 +0100

    v100

--- graphscripts_experiments/relevance_k_fold_few_shot/mutant_train_fine_tune_eval_1.sh
+++ graphscripts_experiments/relevance_k_fold_few_shot/mutant_train_fine_tune_eval_1.sh
@@ -32,13 +32,13 @@ done
 done
 
 
-for train_project in  csv  io lang text collections 
-do
-for fine_tune in no
-do
-    output_folder=results/few_shot_relevance_fine_tune_${fine_tune}/mutants_${num_class}_train_${train_project}_fine_tune_${fine_tune}/context
-    bash eval.sh gat "pretrained_models/context/gat/model_0" ${output_folder}/ attention $device $num_class $train_project results/transfer_siamese_relevance/mutants_2_rm_${train_project}/context/gat/saved_model.pt
-done
-done
+# for train_project in  csv  io lang text collections 
+# do
+# for fine_tune in no
+# do
+#     output_folder=results/few_shot_relevance_fine_tune_${fine_tune}/mutants_${num_class}_train_${train_project}_fine_tune_${fine_tune}/context
+#     bash eval.sh gat "pretrained_models/context/gat/model_0" ${output_folder}/ attention $device $num_class $train_project results/transfer_siamese_relevance/mutants_2_rm_${train_project}/context/gat/saved_model.pt
+# done
+# done
 
 
