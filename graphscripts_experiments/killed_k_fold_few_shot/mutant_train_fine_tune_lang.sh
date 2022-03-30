7e1c0727a3fd324cbf0c8cd518f7040f72abe23e
Marvinmw <1300623733@qq.com>
2022-03-30 20:11:54 +0200

    path fix

--- graphscripts_experiments/killed_k_fold_few_shot/mutant_train_fine_tune_lang.sh
+++ graphscripts_experiments/killed_k_fold_few_shot/mutant_train_fine_tune_lang.sh
@@ -19,7 +19,7 @@ do
 for fine_tune in yes no
 do
     output_folder=results/few_shot_killed_fine_tune_${fine_tune}/mutants_${num_class}_train_${train_project}_fine_tune_${fine_tune}/context
-    load_model=results/transfer_siamese_killed/mutants_2_rm_${train_project}/context/gat/saved_model.pt
+    load_model=results/transfer_siamese_killed/mutants_2_rm_${train_project}/context/gat/
     bash run.sh gat "pretrained_models/context/gat/model_0" ${output_folder}/ attention $device $num_class $train_project $fine_tune $load_model
 done
 done
