de63204b96f4f5b9dfed07283b9fecf6d3627631
Marvinmw <1300623733@qq.com>
2022-04-05 11:53:36 +0200

    yes no

--- graphscripts_experiments/relevance_k_fold_few_shot/mutant_train_fine_tune.sh
+++ graphscripts_experiments/relevance_k_fold_few_shot/mutant_train_fine_tune.sh
@@ -16,7 +16,7 @@ num_class=2
 
 for train_project in text collections csv io lang 
 do
-for fine_tune in yes
+for fine_tune in yes no
 do
     output_folder=results/few_shot_relevance_fine_tune_${fine_tune}/mutants_${num_class}_train_${train_project}_fine_tune_${fine_tune}/context
     bash run.sh gat "pretrained_models/context/gat/model_0" ${output_folder}/ attention $device $num_class $train_project $fine_tune results/transfer_siamese_relevance/mutants_2_rm_${train_project}/context/gat/
