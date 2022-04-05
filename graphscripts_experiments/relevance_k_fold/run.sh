b3159b4db7cf41baf968e1445b234753c55da96e
Marvinmw <1300623733@qq.com>
2022-04-05 09:36:43 +0200

    mkdir

--- graphscripts_experiments/relevance_k_fold/run.sh
+++ graphscripts_experiments/relevance_k_fold/run.sh
@@ -14,7 +14,7 @@ output=${output_prefix}/${gnn_type}
 sw=lstm
 jk=sum
 lstm_emb_dim=150
-#mkdir -p $output
+mkdir -p $output
 python mutants_supervised_one_projects.py --batch_size 256 --num_workers 5  --epochs 10 --num_layer 5 \
 --subword_embedding  $sw \
 --lstm_emb_dim $lstm_emb_dim \
