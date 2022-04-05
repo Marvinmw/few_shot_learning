685b962aace50fc76b8769862a66b2e72ae11efe
Marvinmw <1300623733@qq.com>
2022-04-05 09:42:20 +0200

    data

---
+++ graphscripts_experiments/relevance_k_fold/job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+bash mutant_train_scratch_collections.sh
+bash mutant_train_scratch_csv.sh
+bash mutant_train_scratch_io.sh
+bash mutant_train_scratch_lang.sh
+bash mutant_train_scratch_text.sh
